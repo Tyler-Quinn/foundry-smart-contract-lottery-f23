82a27cdddc2fab8dd28bc9cb9feee6a8488ca1c7
Tyler Quinn <tylquinn16@gmail.com>
2024-03-10 23:08:59 -0400

    refactor to take either native eth or erc20

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.18;
 
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
  * @title A sample Raffle Contract
@@ -12,10 +14,13 @@ import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2
  * @dev Implements Chainlink VRFv2
  */
 contract Raffle is VRFConsumerBaseV2 {
+    using SafeERC20 for IERC20;
+
     /* ERRORS */
     error Raffle_NotEnoughEthSent();
-    error Raffle_TransferFailed();
+    error Raffle_NotEnoughTokenBalance();
     error Raffle_RaffleNotOpen();
+    error Raffle_TransferFailed();
     error Raffle_UpkeepNotNeeded(
         uint256 currentBalance,
         uint256 numPlayers,
@@ -35,6 +40,7 @@ contract Raffle is VRFConsumerBaseV2 {
     bytes32 private immutable i_gasLane;
     uint64 private immutable i_subscriptionId;
     uint32 private immutable i_callbackGasLimit;
+    address private immutable i_raffleToken;
 
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
@@ -53,7 +59,8 @@ contract Raffle is VRFConsumerBaseV2 {
         address vrfCoordinator,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        address raffleToken
     ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
@@ -61,6 +68,7 @@ contract Raffle is VRFConsumerBaseV2 {
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        i_raffleToken = raffleToken;
 
         s_raffleState = RaffleState.OPEN;
         s_lastTimeStamp = block.timestamp;
@@ -70,18 +78,28 @@ contract Raffle is VRFConsumerBaseV2 {
     
      */
     function enterRaffle() external payable {
-        if (msg.value < i_entranceFee) {
-            revert Raffle_NotEnoughEthSent();
-        }
         if (s_raffleState != RaffleState.OPEN) {
             revert Raffle_RaffleNotOpen();
         }
+
+        // if i_raffleToken is zero then the raffle is for native ETH
+        if (i_raffleToken == address(0)) {
+            if (msg.value < i_entranceFee) {
+                revert Raffle_NotEnoughEthSent();
+            }
+        } else {
+            if (IERC20(i_raffleToken).balanceOf(msg.sender) < i_entranceFee) {
+                revert Raffle_NotEnoughTokenBalance();
+            }
+            IERC20(i_raffleToken).safeTransferFrom(msg.sender, address(this), i_entranceFee);
+        }      
+
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
     }
 
     /**
-     * @dev This is the functio that the Chainlink Automation nodes call
+     * @dev This is the function that the Chainlink Automation nodes call
      * to see if the it's time to perform an upkeep.
      * The following should be true for this to return true:
      * 1. The time interval has passed between raffle runs
@@ -94,7 +112,12 @@ contract Raffle is VRFConsumerBaseV2 {
     ) public view returns (bool upkeepNeeded, bytes memory /*performData*/) {
         bool timeHasPassed = (block.timestamp - s_lastTimeStamp) >= i_interval;
         bool isOpen = (RaffleState.OPEN == s_raffleState);
-        bool hasBalance = address(this).balance > 0;
+        bool hasBalance;
+        if (i_raffleToken == address(0)) {
+            hasBalance = address(this).balance > 0;
+        } else {
+            hasBalance = IERC20(i_raffleToken).balanceOf(address(this)) > 0;
+        }
         bool hasPlayers = s_players.length > 0;
         upkeepNeeded = (timeHasPassed && isOpen && hasBalance && hasPlayers);
         return (upkeepNeeded, "0x0");
@@ -106,8 +129,14 @@ contract Raffle is VRFConsumerBaseV2 {
     function performUpkeep(bytes calldata /*performData*/) external {
         (bool upkeepNeeded,) = checkUpkeep("");
         if (!upkeepNeeded) {
+            uint256 balance;
+            if (i_raffleToken == address(0)) {
+                balance = address(this).balance;
+            } else {
+                balance = IERC20(i_raffleToken).balanceOf(address(this));
+            }
             revert Raffle_UpkeepNotNeeded(
-                address(this).balance,
+                balance,
                 s_players.length,
                 uint256(s_raffleState)
             );
@@ -135,9 +164,13 @@ contract Raffle is VRFConsumerBaseV2 {
         s_lastTimeStamp = block.timestamp;
         emit PickedWinner(winner);
 
-        (bool success,) = winner.call{value: address(this).balance}("");
-        if (!success) {
-            revert Raffle_TransferFailed();
+        if (i_raffleToken == address(0)) {
+            (bool success,) = winner.call{value: address(this).balance}("");
+            if (!success) {
+                revert Raffle_TransferFailed();
+            }
+        } else {
+            IERC20(i_raffleToken).safeTransfer(winner, IERC20(i_raffleToken).balanceOf(address(this)));
         }
     }
 
